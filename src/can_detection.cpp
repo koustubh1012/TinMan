41baf9ed12de21c3bdc95ee99c3e20fa55221c58
swaraj <swarajmr@umd.edu>
2024-11-19 19:41:59 -0500

    Task #1.07 Stub implementaion of CanDetection class

---
+++ src/can_detection.cpp
@@ -0,0 +1,19 @@
+#include "can_detection.hpp"
+
+CanDetection::CanDetection() {
+    // Constructor stub
+}
+
+CanDetection::~CanDetection() {
+    // Destructor stub
+}
+
+float CanDetection::trackCentroid() {
+    // Stub for tracking the centroid of detected objects
+    return 0.0;
+}
+
+std::vector<std::string> CanDetection::getDetectedObjects() {
+    // Stub for retrieving detected objects
+    return {};
+}
