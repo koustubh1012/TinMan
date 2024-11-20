32900bf04a098c49bc4381b9ad8cec752bbb3db5
swaraj <swarajmr@umd.edu>
2024-11-19 19:43:43 -0500

    Task #1.09 Stub implementation of TinMan class

---
+++ src/tinman.cpp
@@ -0,0 +1,17 @@
+#include "tinman.hpp"
+
+TinMan::TinMan() : cans_collected(0), cans_trashed(0) {
+    // Constructor stub
+}
+
+void TinMan::collectCans() {
+    // Stub for starting can collection
+}
+
+void TinMan::disposeCans() {
+    // Stub for disposing collected cans
+}
+
+void TinMan::startNavigation() {
+    // Stub for starting navigation
+}
